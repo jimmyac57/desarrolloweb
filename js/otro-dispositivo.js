55d5983923f74b96f07650caf02d17cf540f40b0
Jimmy Aguilera <jimmyaguilera010699@gmail.com>
2024-09-04 15:00:33 -0400

    feat: add fieldset and number of devide to all new devices

--- js/otro-dispositivo.js
+++ js/otro-dispositivo.js
@@ -5,9 +5,9 @@ function generateUniqueId(prefix) {
     return `${prefix}_${Date.now()}_${counter}`;
 }
 
-const crearInputs = (id, id2,id3,id4,id5,id6) => {
-    const html= `<label for=${id} ><em>Nombre del dispositivo:</em></label>
-          <input type="text" id=${id} name="dispositivo[]" required><span></span><br><br>
+const crearInputs = (id, id2,id3,id4,id5,id6,numerodispositivo) => {
+    const html= `<fieldset><legend> dispositivo numero ${numerodispositivo} </legend><label for=${id} ><em>Nombre del dispositivo:</em></label>
+          <input type="text" id=${id} name="dispositivo[]" minlength="3" maxlength=80 size="80" required><span></span><br><br>
     <label for=${id2}>Descripción:</label>
           <textarea id=${id2} rows="4" cols="50" name="descripcion" required></textarea><span></span><br><br>
 
@@ -41,12 +41,16 @@ const crearInputs = (id, id2,id3,id4,id5,id6) => {
 
           <label for="${id6}">Fotos del producto:</label>
           <input type="file" id=${id6} name="archivos[]" multiple required><span></span><br><br>
+
+          </fieldset>
         `;
     return html;
 }
 
+let numerodispositivo = 2;
+
 document.getElementById('addDeviceBtn').addEventListener('click', () => {
-    const contenedorDispositivo = document.getElementById('contenedor-dispositivo');
+    const contenedorDispositivo= document.getElementById('contenedor-dispositivo');
     
     // Generar IDs únicos para cada nuevo dispositivo
     const id = generateUniqueId("dispositivo");
@@ -57,7 +61,7 @@ document.getElementById('addDeviceBtn').addEventListener('click', () => {
     const id6 =   generateUniqueId("archivos");
 
     // Generar HTML para el nuevo dispositivo
-    const nuevoDispositivoHTML = crearInputs(id, id2, id3, id4, id5,id6);
+    const nuevoDispositivoHTML = crearInputs(id, id2, id3, id4, id5,id6,numerodispositivo);
     
     // Crear un nuevo contenedor y añadir el HTML generado
     const nuevoDispositivo = document.createElement('div');
@@ -66,5 +70,7 @@ document.getElementById('addDeviceBtn').addEventListener('click', () => {
     
     // Añadir el nuevo dispositivo al contenedor principal
     contenedorDispositivo.appendChild(nuevoDispositivo);
+
+    numerodispositivo++;
 });
 
