4f1e30032aa8359571d2047b9bf9ed2a55f871b2
Jimmy Aguilera <jimmyaguilera010699@gmail.com>
2024-09-09 15:55:48 -0300

    cleanup: remove files with updated paths

--- js/ver-dispositivos.js
+++
@@ -1,6 +0,0 @@
-//para hacer que solo se muestre el dispositivo seleccionado en informacion-dispositivos.html
-const redirect = (fila) => {
-    const deviceId = fila.getAttribute('data-id');
-    window.location.href = `informacion-dispositivos.html#${deviceId}`;
-}
-
